8624ae2729e635df53e3ce6177c560fb027663f9
Alex Thompson <alexthomps11@gmail.com>
2019-08-20 17:31:42 -0400

    full data for backend

--- client/src/components/RatingCard.js
+++ client/src/components/RatingCard.js
@@ -17,11 +17,16 @@ class RatingCard extends Component {
   handleFavorite = async e => {
     e.preventDefault();
     console.log(this.props.cardData);
-    const { name } = this.props.cardData[0].name;
-    await addRestaurant({
-      userId: this.props.userId,
-      cardData: this.props.cardData
-    });
+    const restObj = {
+      name: this.props.cardData[0].name,
+      address: this.props.cardData[0].location.address1,
+      rating: parseInt(
+        ((this.props.yelpData + this.props.foursquareData) / 2).toFixed(1)
+      )
+    };
+    console.log(this.props.userId);
+    console.log(restObj);
+    await addRestaurant(this.props.userId, restObj);
   };
 
   render() {
